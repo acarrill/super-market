96343a20ec99309b4a1c455829e7d43d05f2efef
acarrill <a.jimenezcarr@alumnos.urjc.es>
2025-04-13 13:08:17 +0200

    feat: add order-manager microservice initialized

---
+++ order-manager/src/test/java/com/mercadona/order_manager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.order_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
